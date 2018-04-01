730bf165cf78ffafbf2e6a30848c4bbc7b3b6b81
TheFox <krakaka088@gmail.com>
2018-04-01 03:34:52 +0300

    Alpha 0.5 - Gimme Context
    -changed the bot check system to be a little less tedious (read about it here https://github.com/thefoxbot/Discord.Console/wiki/How-to-login-as-a-user)
    -made it so you would fetch 10-20 messages before entering a channel (options for that coming in a future update, currently set to 10)

--- discordclient.js
+++ discordclient.js
@@ -1,96 +1,117 @@
  var Discord = require("discord.js");
  var bot = new Discord.Client();
- var input = require("input")
- var accounts = require("./accs.json")
- var fs = require("fs")
- let token
- var chatchannelDefined
- var version = "Alpha v0.41"
+ var input = require("input");
+ var accounts = require("./accs.json");
+ var fs = require("fs");
+ let token;
+ var chatchannelDefined;
+ var version = "Alpha v0.5";
+
+
 
  async function auth() {
-     console.clear()
+     console.clear();
      if(accounts.length===0) {
-        console.log("No accounts present, creating a new one")
-        token = await input.text("Token (ONLY USED TO LOGIN, won't share anywhere)")
-        account = {"token":token}
+        console.log("No accounts present, creating a new one");
+        token = await input.text("Token (ONLY USED TO LOGIN, won't share anywhere)");
+        account = {"token":token,"botCheck":true};
      } else {
-         var accountList = accounts.map(a=>a.username+"#"+a.discrim)
-         accountList.push("New..")
-        account = await input.select("Select an account", accountList)
+         var accountList = accounts.map(a=>a.username+"#"+a.discrim);
+         accountList.push("New..");
+        account = await input.select("Select an account", accountList);
         if(account !== "New..") {
             account = accounts.find(function(element) {
-                return element.username+"#"+element.discrim === account
-            })
-            token = account.token
+                return element.username+"#"+element.discrim === account;
+            });
+            token = account.token;
         } else {
-            console.log("Creating new account!")
-            token = await input.text("Token (ONLY USED TO LOGIN, won't share anywhere)")
-            account = {"token":token}
+            console.log("Creating new account!");
+            token = await input.text("Token (ONLY USED TO LOGIN, won't share anywhere)");
+            account = {"token":token};
         }
      }
  console.log("Logging in...")
- await bot.login(token)
+ await bot.login(token);
  }
 
  async function ready() {
-    console.log("Logged in!")
-    //This is the code that you want to look for!
-    if(!bot.user.bot) {
-        console.log("I'm sorry, but you aren't a bot! Here theres only bots allowed.\nIf you want more info, read the official discord TOS. It is stated that you cannot modify and/or make a new client. This does not count for bots, though.\nIf you still wish to break the TOS and use the client as a normal user, read the \"How to login as a user\" section in the GitHub wiki.")
-        process.exit()
+    console.log("Logged in!");
+	if(account["username"] === undefined) {
+        var save = await input.confirm("Save account?");
+       if(save) {
+       console.log("Setting up account..");
+       account["username"] = bot.user.username;
+       account["discrim"] = bot.user.discriminator;
+       account["id"] = bot.user.id;
+       accounts.push(account);
+       console.log("Saving account..");
+       fs.writeFile("./accs.json", JSON.stringify(accounts));
+       console.log("Done!");
+   }}
+   
+    if(account["botCheck"] === undefined) {
+      account["botCheck"] = true;
+	  if(!account["username"] === undefined) {
+	  fs.writeFile("./accs.json", JSON.stringify(accounts));
+	  }
+    }
+    
+    if(!bot.user.bot && account.botCheck) {
+        console.log("I'm sorry, but you aren't a bot! Here theres only bots allowed.\nIf you want more info, read the official discord TOS. It is stated that you cannot modify and/or make a new client. This does not count for bots, though.\nIf you still wish to break the TOS and use the client as a normal user, read the \"How to login as a user\" section in the GitHub wiki.");
+        process.exit();
     }
 
    
     if(account["username"] === undefined) {
-        var save = await input.confirm("Save account?")
+        var save = await input.confirm("Save account?");
        if(save) {
-       console.log("Setting up account..")
-       account["username"] = bot.user.username
-       account["discrim"] = bot.user.discriminator
-       account["id"] = bot.user.id
-       accounts.push(account)
-       console.log("Saving account..")
-       fs.writeFile("./accs.json", JSON.stringify(accounts))
-       console.log("Done!")
+       console.log("Setting up account..");
+       account["username"] = bot.user.username;
+       account["discrim"] = bot.user.discriminator;
+       account["id"] = bot.user.id;
+       accounts.push(account);
+       console.log("Saving account..");
+       fs.writeFile("./accs.json", JSON.stringify(accounts));
+       console.log("Done!");
    }} else {
-       console.log("Welcome back, "+account.username)
+       console.log("Welcome back, "+account.username);
    }
-    console.log("Starting Discord.Console "+version+"!!!")
-    mainMenu()
+    console.log("Starting Discord.Console "+version+"!!!");
+    mainMenu();
  }
 
- bot.on("ready", ready)
+ bot.on("ready", ready);
 
  async function mainMenu() {
-     console.clear()
-     console.log("Logged in as "+account.username+"#"+account.discrim)
-    var option = await input.select("Discord.Console "+version+" Main Menu", ["Go to chat!","DMs","Switch account","Quit"])
+     console.clear();
+     console.log("Logged in as "+account.username+"#"+account.discrim);
+    var option = await input.select("Discord.Console "+version+" Main Menu", ["Go to chat!","DMs","Switch account","Quit"]);
     switch(option) {
         case "Go to chat!":
-        selectChannel()
+        selectChannel();
         break;
         case "DMs":
-        selectUser()
+        selectUser();
         break;
         case "Switch account":
-        bot.destroy()
-        auth()
+        bot.destroy();
+        auth();
         break;
         case "Account options":
-        accOptions()
+        accOptions();
         break;
         case "Quit":
-        process.exit()
+        process.exit();
         break;
     }
  }
 
  async function selectChannel() {
-     console.clear()
-     var guildsmap = ["Switch to User search","Skip to channel ID","Search","Quit"]
+     console.clear();
+     var guildsmap = ["Switch to User search","Skip to channel ID","Search","Quit"];
      bot.guilds.map(ch=>ch.name+" - "+ch.id).forEach(function(element){
-        guildsmap.push(element)
-     })
+        guildsmap.push(element);
+     });
      var guildId = await input.select("What guild do you want to look for channels in?", guildsmap)
      switch(guildId) {
          case "Skip to channel ID":
@@ -182,9 +203,13 @@
      chatchannelDefined = true
      userchannel = {"id":null}
      chatchannel = bot.channels.get(channel)
+     console.log("Fetching previous messages...")
      console.log("Making new listener...")
      bot.on("message", msg => {handleChan(msg)})
      console.log("We got it, listening to channel #"+chatchannel.name+"\nType -quit to select a different channel")
+     chatchannel.fetchMessages({ limit: 10 })
+      .then(messages => console.log(messages.map(m=>"! > "+m.author.username+m.author.discriminator+": "+m.content).reverse().forEach(function(message) {console.log(message)})))
+      .catch(console.error);
      while(true) {
      var message = await input.text(">")
      if(message === "-quit") {
@@ -236,7 +261,7 @@ async function handleDM(msg) {
       } else {
          var botTag = ""
        }
-      console.log("\n> DM "+msg.author.username+"#"+msg.author.discriminator+botTag+": "+msg.content)
+      console.log("\n! > DM "+msg.author.username+"#"+msg.author.discriminator+botTag+": "+msg.content)
     }
   }
 }
@@ -251,7 +276,7 @@ async function handleChan(msg) {
       } else {
          var botTag = ""
      }
-      console.log("\n> "+msg.author.username+"#"+msg.author.discriminator+botTag+": "+msg.content)
+      console.log("\n! > "+msg.author.username+"#"+msg.author.discriminator+botTag+": "+msg.content)
     }
   }
 }
